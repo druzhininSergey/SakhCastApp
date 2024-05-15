9a8326ad8f712f4f5b203db99deefcde22edf201
sakhb <druzhininsergey96@gmail.com>
2024-05-15 11:50:00 +0700

    SC-01 add test collors

--- app/src/main/java/com/example/sakhcastapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/sakhcastapplication/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.example.sakhcastapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val charcoalGray = Color(0xFF48464C)
+val coolGray = Color(0xFF938F96)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val pastelLavender = Color(0xFFF3EDF7)
+val lavenderMist = Color(0xFFE6E0E9)
--- app/src/main/java/com/example/sakhcastapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/sakhcastapplication/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.sakhcastapplication.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,25 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = charcoalGray,
+    secondary = coolGray,
+    onPrimary = pastelLavender,
+    onSecondary = lavenderMist,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = pastelLavender,
+    secondary = lavenderMist,
+    onPrimary = charcoalGray,
+    onSecondary = charcoalGray
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -41,10 +33,10 @@ fun SakhCastApplicationTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
